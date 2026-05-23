0becffdfefc822238e2304901c6ea3472fa5e05e
stevenferdy <stevenferdy099@gmail.com>
2026-05-23 22:03:40 +0700

    main_server.cpp

--- kuis_windows/server/main_server.cpp
+++ kuis_windows/server/main_server.cpp
@@ -6,10 +6,3 @@ int main() {
     return 0;
 }
 
-// ============================================================
-//  KOMPILASI DI WINDOWS (MinGW/MSYS2):
-//  g++ -std=c++17 -pthread -I../common main_server.cpp -o server.exe -lws2_32
-//
-//  JALANKAN:
-//  server.exe
-// ============================================================
